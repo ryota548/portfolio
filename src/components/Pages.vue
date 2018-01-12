654a116c92e208e0469b2c0b3f6d76f5f7ad46ca
ryota548 <trb_right9@yahoo.co.jp>
2018-01-12 23:17:55 +0900

    [update]

---
+++ src/components/Pages.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Hello World</p>
+</template>
+
+<script>
+export default {
+  name: 'Pages'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
