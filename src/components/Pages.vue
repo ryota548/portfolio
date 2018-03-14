8a0d5b23d35c1d460c87b0004ad7fc2181670d17
ryota548 <trb_right9@yahoo.co.jp>
2018-03-14 23:54:02 +0900

    set center of width

--- src/components/Pages.vue
+++ src/components/Pages.vue
@@ -1,9 +1,13 @@
 <template>
-<div>
-  <p>Hello World</p>
-  <router-link to="/aboutme">about me</router-link>
-  <router-link to="/job">job</router-link>
-  <router-link to="/output">output</router-link>
+<div class="top">
+  <div class="title">
+    <span>ryota koshiba</span>
+  </div>
+  <div class="contents">
+    <router-link to="/aboutme">about me</router-link>
+    <router-link to="/job">job</router-link>
+    <router-link to="/output">output</router-link>
+  </div>
 </div>
 </template>
 
@@ -15,5 +19,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.top {
+  height: 100%;
+  max-width: 200px;
+  margin: auto;
+  background-color: greenyellow;
+}
+
+title, .contents {
+  width: 100%;
+}
+
+.title {
+  background-color: red;
+}
+
+span {
+  width: 150px;
+  color: white;
+  margin: auto;
+}
+
+.contents {
+}
 
 </style>
