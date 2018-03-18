bc6fc1a1991950258cd8e0015d1ce1d1c5642aff
ryota548 <trb_right9@yahoo.co.jp>
2018-03-18 15:42:30 +0900

    modify abotme

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,7 +1,9 @@
 <template>
-<div>
-  <p>AboutMe</p>
-  <router-link to="/">to top</router-link>
+<div class="aboutme">
+    <router-link to="/" class="top__logo">ryota koshiba</router-link>
+    <p class="aboutme__block">dmm.com</p>
+    <p class="aboutme__block">naist</p>
+    <p class="aboutme__block">doshisha</p>
 </div>
 </template>
 
@@ -12,6 +14,24 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+
+.aboutme {
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template-rows: 45px 1fr; 
+  grid-template-columns: 33% 33% 1fr;
+  box-sizing: border-box;
+  grid-gap:5px;
+
+  text-align: center;
+  vertical-align: middle;
+
+  &__block {  
+    background-color: lightpink;
+  }
+}
 
 </style>
